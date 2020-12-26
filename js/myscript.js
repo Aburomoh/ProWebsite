b0d2f2acc4f71ab6dff8cc7af79c6fad9db8a581
Aburomoh <76401257+Aburomoh@users.noreply.github.com>
2020-12-27 00:41:24 +0100

    update

--- js/myscript.js
+++ js/myscript.js
@@ -6,7 +6,7 @@ function myFunction(){
 	console.log(window.scrollY);
 	var MaxEffect = 400;
 	//document.getElementById("banner").style.borderRadius = window.scrollY/5 + "vw";
-	document.getElementById("banner").style.opacity = 1-window.scrollY/MaxEffect;
+	document.getElementsByClassName("banner")[0].style.opacity = 1-window.scrollY/MaxEffect;
 	document.getElementsByTagName("nav")[0].style.opacity = window.scrollY/MaxEffect;
 	document.getElementById("scrollup").style.bottom = Math.min(window.scrollY/12,MaxEffect/12) + "vh";
 	//document.getElementById("banner").style.height = (120 - window.scrollY/4) + "vw";
