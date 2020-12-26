f72a82d886948b6dcb7481510c3af75e5b4342b1
Aburomoh <76401257+Aburomoh@users.noreply.github.com>
2020-12-27 00:20:14 +0100

    Last update

--- js/myscript.js
+++ js/myscript.js
@@ -8,6 +8,6 @@ function myFunction(){
 	//document.getElementById("banner").style.borderRadius = window.scrollY/5 + "vw";
 	document.getElementById("banner").style.opacity = 1-window.scrollY/MaxEffect;
 	document.getElementsByTagName("nav")[0].style.opacity = window.scrollY/MaxEffect;
-	document.getElementById("scrollup").style.bottom = Math.min(window.scrollY/12,65) + "vh";
+	document.getElementById("scrollup").style.bottom = Math.min(window.scrollY/12,MaxEffect/12) + "vh";
 	//document.getElementById("banner").style.height = (120 - window.scrollY/4) + "vw";
 }
