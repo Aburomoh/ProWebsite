6127cdba4fc4950fa3c01e67068f51584e4df6f7
Aburomoh <76401257+Aburomoh@users.noreply.github.com>
2020-12-26 22:08:03 +0100

    Update
    visual

--- js/myscript.js
+++ js/myscript.js
@@ -6,6 +6,6 @@ function myFunction(){
 	console.log(window.scrollY);
 	//document.getElementById("banner").style.borderRadius = window.scrollY/5 + "vw";
 	document.getElementById("banner").style.opacity = 1-window.scrollY/400;
-
+	document.getElementsByTagName("nav")[0].style.opacity = window.scrollY/400;
 	//document.getElementById("banner").style.height = (120 - window.scrollY/4) + "vw";
 }
