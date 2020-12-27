1fc7e200e664eaf504c437d52ff64b0fceb09a74
Aburomoh <76401257+Aburomoh@users.noreply.github.com>
2020-12-27 02:51:44 +0100

    Update myscript.js

--- js/myscript.js
+++ js/myscript.js
@@ -4,7 +4,7 @@ function myscrollFunction(){
 	//console.log(window.scrollY);
 	//document.getElementById("banner").style.borderRadius = window.scrollY/5 + "vw";
 
-	document.getElementsByClassName("banner")[0].style.opacity = 1-0.5*window.scrollY/MaxEffect;
+	document.getElementsByClassName("banner")[0].style.opacity = 1-0.9*window.scrollY/MaxEffect;
 	document.getElementsByTagName("nav")[0].style.opacity = window.scrollY/MaxEffect;
 
 	if (100-(window.scrollY/MaxEffect*100)>0) {
